d28f25d7a76d5e2bb4ad067c7673420ffe089169
ABHISHEK DAS <abhishek999.ad@gmail.com>
2020-09-18 19:53:34 +0530

    Add files via upload

---
+++ outer.py
@@ -0,0 +1,9 @@
+import numpy as np
+p = [[1, 0], [0, 1]]
+q = [[1, 2], [3, 4]]
+print("original matrix:")
+print(p)
+print(q)
+result = np.outer(p, q)
+print("Outer product of the said two vectors:")
+print(result)
